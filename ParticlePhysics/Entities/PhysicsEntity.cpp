dd5604f4c614ecee286d2e6af05f118acc3f5b48
vivektri88 <vivek.tri88@gmail.com>
2019-01-12 21:42:52 -0500

    Fixing issue with collision data display

--- ParticlePhysics/Entities/PhysicsEntity.cpp
+++ ParticlePhysics/Entities/PhysicsEntity.cpp
@@ -12,12 +12,13 @@ PhysicsEntity::PhysicsEntity()
 
   ParticleSharedData sharedData;
   sharedData.velocityDamping = .98f;
-  sharedData.invMassIsShared = 0;
+  sharedData.invMassIsShared = 1;
   sharedData.sharedInvMass = 0.f;
   sharedData.radiusIsShared = 1;
   sharedData.sharedRadius = 0.f;
   sharedData.collisionDataIsShared = 1;
   sharedData.sharedCollisionData.sdfGradient = float3(0, 0, 0);
+  sharedData.sharedCollisionData.sdfMagnitude = 0.f;
   sharedData.sharedCollisionData.sdfGradient2 = float3(0, 0, 0);
   entitySharedData.host()->reserve(1);
   entitySharedData.host()->push_back(sharedData);
