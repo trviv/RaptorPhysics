1a8dc77c9d35a980695307b63907b9210853d50d
vivektri88 <vivek.tri88@gmail.com>
2019-04-21 16:18:47 -0400

    Increased batch size for parallel primitives for better performance

--- Core/Header/ComputeUtilsShared.h
+++ Core/Header/ComputeUtilsShared.h
@@ -60,14 +60,17 @@
 #endif
 
 #if MemberStructType == uint
+#define MemberStructType16 uint16
 #define MemberStructType8 uint8
 #define MemberStructType4 uint4
 #define MemberStructType2 uint2
 #elif MemberStructType == int
+#define MemberStructType16 int16
 #define MemberStructType8 int8
 #define MemberStructType4 int4
 #define MemberStructType2 int2
 #elif MemberStructType == float
+#define MemberStructType16 float16
 #define MemberStructType8 float8
 #define MemberStructType4 float4
 #define MemberStructType2 float2
@@ -94,7 +97,9 @@ void batchRead(Thread MemberStructType *elements, const Device StructType* array
   StructType data[BatchSize];
 #endif
 
-#if BatchSize == 8
+#if BatchSize == 16
+  * ((Thread MemberStructType16*)elements) = (MemberStructType16)(0);
+#elif BatchSize == 8
   * ((Thread MemberStructType8*)elements) = (MemberStructType8)(0);
 #elif BatchSize == 4
   * ((Thread MemberStructType4*)elements) = (MemberStructType4)(0);
@@ -104,51 +109,105 @@ void batchRead(Thread MemberStructType *elements, const Device StructType* array
 
   switch (readCount)
   {
+#if BatchSize >= 16
+  case 16:
+  {
+    *((Thread MemberStructType16*)data)       = *((const Device MemberStructType16*)(array1D + indexOffset));
+    break;
+  }
+  case 15:
+  {
+    *((Thread MemberStructType8*)(data + 0))  = *((const Device MemberStructType8*)(array1D + indexOffset));
+    *((Thread MemberStructType4*)(data + 8))  = *((const Device MemberStructType4*)(array1D + indexOffset + 8));
+    *((Thread MemberStructType2*)(data + 12)) = *((const Device MemberStructType2*)(array1D + indexOffset + 12));
+    *((Thread MemberStructType*)(data + 14))  = *((const Device MemberStructType*)(array1D + indexOffset + 14));
+    break;
+  }
+  case 14:
+  {
+    *((Thread MemberStructType8*)(data + 0))  = *((const Device MemberStructType8*)(array1D + indexOffset));
+    *((Thread MemberStructType4*)(data + 8))  = *((const Device MemberStructType4*)(array1D + indexOffset + 8));
+    *((Thread MemberStructType2*)(data + 12)) = *((const Device MemberStructType2*)(array1D + indexOffset + 12));
+    break;
+  }
+  case 13:
+  {
+    *((Thread MemberStructType8*)(data + 0))  = *((const Device MemberStructType8*)(array1D + indexOffset));
+    *((Thread MemberStructType4*)(data + 8))  = *((const Device MemberStructType4*)(array1D + indexOffset + 8));
+    *((Thread MemberStructType*)(data + 12))  = *((const Device MemberStructType*)(array1D + indexOffset + 12));
+    break;
+  }
+  case 12:
+  {
+    *((Thread MemberStructType8*)(data + 0))  = *((const Device MemberStructType8*)(array1D + indexOffset));
+    *((Thread MemberStructType4*)(data + 8))  = *((const Device MemberStructType4*)(array1D + indexOffset + 8));
+    break;
+  }
+  case 11:
+  {
+    *((Thread MemberStructType8*)(data + 0))  = *((const Device MemberStructType8*)(array1D + indexOffset));
+    *((Thread MemberStructType2*)(data + 8))  = *((const Device MemberStructType2*)(array1D + indexOffset + 8));
+    *((Thread MemberStructType*)(data + 10))  = *((const Device MemberStructType*)(array1D + indexOffset + 10));
+    break;
+  }
+  case 10:
+  {
+    *((Thread MemberStructType8*)(data + 0))  = *((const Device MemberStructType8*)(array1D + indexOffset));
+    *((Thread MemberStructType2*)(data + 8))  = *((const Device MemberStructType2*)(array1D + indexOffset + 8));
+    break;
+  }
+  case 9:
+  {
+    *((Thread MemberStructType8*)(data + 0))  = *((const Device MemberStructType8*)(array1D + indexOffset));
+    *((Thread MemberStructType*)(data + 8))   = *((const Device MemberStructType*)(array1D + indexOffset + 8));
+    break;
+  }
+#endif
 #if BatchSize >= 8
   case 8:
   {
-    *((Thread MemberStructType8*)data) = *((const Device MemberStructType8*)(array1D + indexOffset));
+    *((Thread MemberStructType8*)data + 0)    = *((const Device MemberStructType8*)(array1D + indexOffset));
     break;
   }
   case 7:
   {
-    *((Thread MemberStructType4*)(data + 0)) = *((const Device MemberStructType4*)(array1D + indexOffset));
-    *((Thread MemberStructType2*)(data + 4)) = *((const Device MemberStructType2*)(array1D + indexOffset + 4));
-    *((Thread MemberStructType*)(data + 6)) = *((const Device MemberStructType*)(array1D + indexOffset + 6));
+    *((Thread MemberStructType4*)(data + 0))  = *((const Device MemberStructType4*)(array1D + indexOffset));
+    *((Thread MemberStructType2*)(data + 4))  = *((const Device MemberStructType2*)(array1D + indexOffset + 4));
+    *((Thread MemberStructType*)(data + 6))   = *((const Device MemberStructType*)(array1D + indexOffset + 6));
     break;
   }
   case 6:
   {
-    *((Thread MemberStructType4*)(data + 0)) = *((const Device MemberStructType4*)(array1D + indexOffset));
-    *((Thread MemberStructType2*)(data + 4)) = *((const Device MemberStructType2*)(array1D + indexOffset + 4));
+    *((Thread MemberStructType4*)(data + 0))  = *((const Device MemberStructType4*)(array1D + indexOffset));
+    *((Thread MemberStructType2*)(data + 4))  = *((const Device MemberStructType2*)(array1D + indexOffset + 4));
     break;
   }
   case 5:
   {
-    *((Thread MemberStructType4*)(data + 0)) = *((const Device MemberStructType4*)(array1D + indexOffset));
-    *((Thread MemberStructType*)(data + 4)) = *((const Device MemberStructType*)(array1D + indexOffset + 4));
+    *((Thread MemberStructType4*)(data + 0))  = *((const Device MemberStructType4*)(array1D + indexOffset));
+    *((Thread MemberStructType*)(data + 4))   = *((const Device MemberStructType*)(array1D + indexOffset + 4));
     break;
   }
 #endif
   case  4:
   {
-    *((Thread MemberStructType4*)data) = *((const Device MemberStructType4*)(array1D + indexOffset));
+    *((Thread MemberStructType4*)data + 0)    = *((const Device MemberStructType4*)(array1D + indexOffset));
     break;
   }
   case 3:
   {
-    *((Thread MemberStructType2*)(data + 0)) = *((const Device MemberStructType2*)(array1D + indexOffset));
-    *((Thread MemberStructType*)(data + 2)) = *((const Device MemberStructType*)(array1D + indexOffset + 2));
+    *((Thread MemberStructType2*)(data + 0))  = *((const Device MemberStructType2*)(array1D + indexOffset));
+    *((Thread MemberStructType*)(data + 2))   = *((const Device MemberStructType*)(array1D + indexOffset + 2));
     break;
   }
   case 2:
   {
-    *((Thread MemberStructType2*)data) = *((const Device MemberStructType2*)(array1D + indexOffset));
+    *((Thread MemberStructType2*)data + 0)    = *((const Device MemberStructType2*)(array1D + indexOffset));
     break;
   }
   case 1:
   {
-    *((Thread MemberStructType*)data) = *((const Device MemberStructType*)(array1D + indexOffset));
+    *((Thread MemberStructType*)data + 0)     = *((const Device MemberStructType*)(array1D + indexOffset));
     break;
   }
   default:
@@ -174,51 +233,105 @@ void batchWrite(const MemberStructType *elements, Device StructType* array1D, co
 
   switch (writeCount)
   {
+#if BatchSize >= 16
+  case 16:
+  {
+    *((Device MemberStructType16*)(array1D + indexOffset))      = *((const Thread MemberStructType16*)elements);
+    break;
+  }
+  case 15:
+  {
+    *((Device MemberStructType8*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType8*)(elements));
+    *((Device MemberStructType4*)(array1D + indexOffset + 8))   = *((const Thread MemberStructType4*)(elements + 8));
+    *((Device MemberStructType2*)(array1D + indexOffset + 12))  = *((const Thread MemberStructType2*)(elements + 12));
+    *((Device MemberStructType*)(array1D + indexOffset + 14))   = *((const Thread MemberStructType*)(elements + 14));
+    break;
+  }
+  case 14:
+  {
+    *((Device MemberStructType8*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType8*)(elements));
+    *((Device MemberStructType4*)(array1D + indexOffset + 8))   = *((const Thread MemberStructType4*)(elements + 8));
+    *((Device MemberStructType2*)(array1D + indexOffset + 12))  = *((const Thread MemberStructType2*)(elements + 12));
+    break;
+  }
+  case 13:
+  {
+    *((Device MemberStructType8*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType8*)(elements));
+    *((Device MemberStructType4*)(array1D + indexOffset + 8))   = *((const Thread MemberStructType4*)(elements + 8));
+    *((Device MemberStructType*)(array1D + indexOffset + 12))   = *((const Thread MemberStructType*)(elements + 12));
+    break;
+  }
+  case 12:
+  {
+    *((Device MemberStructType8*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType8*)(elements));
+    *((Device MemberStructType4*)(array1D + indexOffset + 8))   = *((const Thread MemberStructType4*)(elements + 8));
+    break;
+  }
+  case 11:
+  {
+    *((Device MemberStructType8*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType8*)(elements));
+    *((Device MemberStructType2*)(array1D + indexOffset + 8))   = *((const Thread MemberStructType2*)(elements + 8));
+    *((Device MemberStructType*)(array1D + indexOffset + 10))   = *((const Thread MemberStructType*)(elements + 10));
+    break;
+  }
+  case 10:
+  {
+    *((Device MemberStructType8*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType8*)(elements));
+    *((Device MemberStructType2*)(array1D + indexOffset + 8))   = *((const Thread MemberStructType2*)(elements + 8));
+    break;
+  }
+  case 9:
+  {
+    *((Device MemberStructType8*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType8*)(elements));
+    *((Device MemberStructType*)(array1D + indexOffset + 8))    = *((const Thread MemberStructType*)(elements + 8));
+    break;
+  }
+#endif
 #if BatchSize >= 8
   case 8:
   {
-    *((Device MemberStructType8*)(array1D + indexOffset)) = *((const Thread MemberStructType8*)elements);
+    *((Device MemberStructType8*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType8*)elements);
     break;
   }
   case 7:
   {
-    *((Device MemberStructType4*)(array1D + indexOffset)) = *((const Thread MemberStructType4*)(elements + 0));
-    *((Device MemberStructType2*)(array1D + indexOffset + 4)) = *((const Thread MemberStructType2*)(elements + 4));
-    *((Device MemberStructType*)(array1D + indexOffset + 6)) = *((const Thread MemberStructType*)(elements + 6));
+    *((Device MemberStructType4*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType4*)(elements + 0));
+    *((Device MemberStructType2*)(array1D + indexOffset + 4))   = *((const Thread MemberStructType2*)(elements + 4));
+    *((Device MemberStructType*)(array1D + indexOffset + 6))    = *((const Thread MemberStructType*)(elements + 6));
     break;
   }
   case 6:
   {
-    *((Device MemberStructType4*)(array1D + indexOffset)) = *((const Thread MemberStructType4*)(elements + 0));
-    *((Device MemberStructType2*)(array1D + indexOffset + 4)) = *((const Thread MemberStructType2*)(elements + 4));
+    *((Device MemberStructType4*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType4*)(elements + 0));
+    *((Device MemberStructType2*)(array1D + indexOffset + 4))   = *((const Thread MemberStructType2*)(elements + 4));
     break;
   }
   case 5:
   {
-    *((Device MemberStructType4*)(array1D + indexOffset)) = *((const Thread MemberStructType4*)(elements + 0));
-    *((Device MemberStructType*)(array1D + indexOffset + 4)) = *((const Thread MemberStructType*)(elements + 4));
+    *((Device MemberStructType4*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType4*)(elements + 0));
+    *((Device MemberStructType*)(array1D + indexOffset + 4))    = *((const Thread MemberStructType*)(elements + 4));
     break;
   }
 #endif
   case 4:
   {
-    *((Device MemberStructType4*)(array1D + indexOffset)) = *((const Thread MemberStructType4*)elements);
+    *((Device MemberStructType4*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType4*)elements);
     break;
   }
   case 3:
   {
-    *((Device MemberStructType2*)(array1D + indexOffset)) = *((const Thread MemberStructType2*)(elements + 0));
-    *((Device MemberStructType*)(array1D + indexOffset + 2)) = *((const Thread MemberStructType*)(elements + 2));
+    *((Device MemberStructType2*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType2*)(elements + 0));
+    *((Device MemberStructType*)(array1D + indexOffset + 2))    = *((const Thread MemberStructType*)(elements + 2));
     break;
   }
   case 2:
   {
-    *((Device MemberStructType2*)(array1D + indexOffset)) = *((const Thread MemberStructType2*)elements);
+    *((Device MemberStructType2*)(array1D + indexOffset + 0))   = *((const Thread MemberStructType2*)elements);
     break;
   }
   case 1:
   {
-    *((Device MemberStructType*)(array1D + indexOffset)) = *((const Thread MemberStructType*)elements);
+    *((Device MemberStructType*)(array1D + indexOffset + 0))    = *((const Thread MemberStructType*)elements);
     break;
   }
   default:
