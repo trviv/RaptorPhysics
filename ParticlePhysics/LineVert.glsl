75195045f35e4ce38fd2ed6a4b95ce0f705699a7
Vivek Trivedi <vivek@viveks-mbp.home>
2019-10-16 19:32:17 -0400

    Removed cpu processing for rendering. Changed default texture width to 128.

--- ParticlePhysics/LineVert.glsl
+++ ParticlePhysics/LineVert.glsl
@@ -2,17 +2,20 @@ uniform mat4 modelViewMatrix;
 uniform mat4 projectionMatrix;
 
 uniform sampler2D particlePos;
-uniform sampler2D particleSDFGrad;
+uniform sampler2D particleCollData;
 
 out vec4 col;
 
+#define texureWidth     128
+#define texureWidthExp  7
+
 void main()
 {
-  vec4 pos = texelFetch(particlePos, ivec2(gl_InstanceID & 0xF, gl_InstanceID >> 4), 0);
+  vec4 pos = texelFetch(particlePos, ivec2(gl_InstanceID & (texureWidth - 1), gl_InstanceID >> texureWidthExp), 0);
 
   if (gl_VertexID>0)
   {
-    vec4 gradient = texelFetch(particleSDFGrad, ivec2(gl_InstanceID & 0xF, gl_InstanceID >> 4), 0);
+    vec4 gradient = texelFetch(particleCollData, ivec2((gl_InstanceID * 2 + 1) & (texureWidth - 1), (gl_InstanceID * 2) >> texureWidthExp), 0);
     pos += vec4(gradient.xyz, 1.0f);
   }
 
