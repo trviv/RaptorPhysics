a4271c5c10225767d484d0ae8ecb7d0e4e9df6a0
Vivek Trivedi <vivek.tri88@gmail.com>
2019-12-19 21:11:06 -0600

    Added some app parameters.

--- App/Main.cpp
+++ App/Main.cpp
@@ -11,40 +11,32 @@ int main(int argc, char** argv)
   compute = new ComputeInterface();
   compute->create();
 
-  physicsSystem = new PhysicsSystem(compute, 256 * 1024);
+  physicsSystem = new PhysicsSystem(compute, 128 * 1024);
   main_window = physicsSystem;
   main_window->init(argc, argv, 1280, 1080);
+  main_window->readCSVFile("AppConfig.csv");
 
-  uint clothEntities = 0;
+  uint clothEntities = 8;
   uint clothInstances = 1;
 
-  uint rigidEntities = 0;
+  uint rigidEntities = 8;
   uint rigidInstances = 100;
 
-  uint fluidEntities = 1;
+  uint fluidEntities = 0;
   uint fluidInstances = 1;
 
   XAB systemBound;
 
-//  systemBound.min = Real3(-1.f, 0.f, -7.f);
-//  systemBound.max = Real3(1.f, 0.f, -5.f);
+  systemBound.min = Real3(Real3(physicsSystem->getParamAsFloat("systemBoundMin", 0),
+                                physicsSystem->getParamAsFloat("systemBoundMin", 1),
+                                physicsSystem->getParamAsFloat("systemBoundMin", 2)));
+  systemBound.max = Real3(Real3(physicsSystem->getParamAsFloat("systemBoundMax", 0),
+                                physicsSystem->getParamAsFloat("systemBoundMax", 1),
+                                physicsSystem->getParamAsFloat("systemBoundMax", 2)));
+  physicsSystem->setGravity(Real3(physicsSystem->getParamAsFloat("gravity", 0),
+                                  physicsSystem->getParamAsFloat("gravity", 1),
+                                  physicsSystem->getParamAsFloat("gravity", 2)));
 
-//  systemBound.min = Real3(-2.f, 0.f, -2.f);
-//  systemBound.max = Real3(2.f, 0.f, 2.f);
-
-  systemBound.min = Real3(-7.f, 0.f, -7.f);
-  systemBound.max = Real3(7.f, 0.f, 7.f);
-
-//  systemBound.min = Real3(-4.f, 0.f, -4.f);
-//  systemBound.max = Real3(4.f, 0.f, 4.f);
-
-//  systemBound.min = Real3(-10.f, 0.f, -10.f);
-//  systemBound.max = Real3(10.f, 0.f, 10.f);
-//
-//  systemBound.min = Real3(-70.f, 0.f, -70.f);
-//  systemBound.max = Real3(70.f, 0.f, 70.f);
-
-  physicsSystem->setGravity(Real3(0.f, -9.8f, 0.f));
   physicsSystem->setSystemBoundary(systemBound);
 
   vector<Matrix4> matrixTransforms;
@@ -56,7 +48,6 @@ int main(int argc, char** argv)
   real rigidDim[3] = {2.0f, 2.0f, 2.0f};
   real rigidSimSize = 2.f;
 
-//  real fluidDim[3] = {.9f, .8f, .9f};
   real fluidDim[3] = {1.2f, 22.5f, 1.5f};
   real fluidSimSize = 2.f;
 
@@ -71,9 +62,6 @@ int main(int argc, char** argv)
     for (int i = 0; i < clothInstances; i++)
     {
       Matrix4 matrix;
-//      float randx = (2 * float(rand()) / RAND_MAX) - 1;
-//      float randy = 2 + float(rand()) / RAND_MAX;
-//      float randz = -5 + (2 * float(rand()) / RAND_MAX) - 1;
 
       float randx = 1.5 * c - clothEntities * 0.5f;
       float randy = 1 + 1.05 * i;
@@ -101,12 +89,8 @@ int main(int argc, char** argv)
     {
       Matrix4 matrix;
       float randx = r * 1.5 - rigidEntities * 0.5f;
-      float randy = 1 + 1.05 * i;
+      float randy = 1 + 1.2 * i;
       float randz = -5;
-
-//      float randx = (5 * float(rand()) / RAND_MAX) - 1;
-//      float randy = 5 + float(rand()) / RAND_MAX;
-//      float randz = (5 * float(rand()) / RAND_MAX) - 1;
 
       matrix.set(Matrix3::getIdentity(), Real3(randx * rigidSimSize, randy * rigidSimSize, randz * rigidSimSize));
 //      Matrix mat;
@@ -130,13 +114,9 @@ int main(int argc, char** argv)
     for (int i = 0; i < fluidInstances; i++)
     {
       Matrix4 matrix;
-      float randx = 0;//f * 1.5 - fluidEntities * 0.5f;
+      float randx = 0;
       float randy = 10.5f + 0.05 * i;
       float randz = 0;
-
-      //      float randx = (5 * float(rand()) / RAND_MAX) - 1;
-      //      float randy = 5 + float(rand()) / RAND_MAX;
-      //      float randz = (5 * float(rand()) / RAND_MAX) - 1;
 
       matrix.set(Matrix3::getIdentity(), Real3(randx * rigidSimSize, randy * rigidSimSize, randz * rigidSimSize));
       //      Matrix mat;
