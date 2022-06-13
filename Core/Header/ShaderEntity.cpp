fdfeb718deaf311573d4e1e3e92c6bff70571677
Vivek Trivedi <5340687+trivedivivek@users.noreply.github.com>
2022-06-12 23:28:27 -0500

    sprintf fixes from mac os. Adding perf explorer

--- Core/Header/ShaderEntity.cpp
+++ Core/Header/ShaderEntity.cpp
@@ -42,3 +42,40 @@ void ShaderEntity::registerShader(ComputeInterface* compute, const char* fileNam
 
   programs.push_back(compute->createTemplateProgram(fileName, &localOld, &localNew, includeFiles));
 }
+
+void ShaderEntity::registerShaderFromSource(ComputeInterface* compute, const string& source, const vector<string>* oldType, const vector<string>* newType, const vector<string>* includeFiles)
+{
+  vector<string> localOld, localNew;
+
+  if (oldType)
+  {
+    localOld.insert(localOld.begin(), oldType->begin(), oldType->end());
+    localNew.insert(localNew.begin(), newType->begin(), newType->end());
+  }
+
+  localOld.push_back("ComputeSimdWidth");
+  localNew.push_back(to_string(compute->simdSize()));
+
+  localOld.push_back("ComputeSimdWidthExp");
+  localNew.push_back(to_string(mCeilExpOf2(compute->simdSize())));
+
+#ifdef USE_METAL_COMPUTE
+  localOld.push_back("USE_METAL_COMPUTE");
+  localNew.push_back("");
+#endif
+
+#ifdef USE_VULKAN_COMPUTE
+  localOld.push_back("USE_METAL_COMPUTE");
+  localNew.push_back("");
+
+  localOld.push_back("USE_SIMD_COMPUTE");
+  localNew.push_back("");
+#endif
+
+#ifdef USE_OPENCL_COMPUTE
+  localOld.push_back("USE_OPENCL_COMPUTE");
+  localNew.push_back("");
+#endif
+
+  programs.push_back(compute->createTemplateProgram(source, &localOld, &localNew, includeFiles));
+}
